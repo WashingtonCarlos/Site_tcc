0b59743b98ddfbdd91b7b8ba693822baa0e443a0
Washington Carlos <wash_28@outlook.com>
2021-03-24 22:49:56 -0300

    delete 2

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,8 +4,11 @@ namespace Frota\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-class Usuario extends Model
+class Usuario extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+   use Authenticatable;
+   public $table = "Usuarios";
 }
