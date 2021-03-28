4ce1d7cb34cacb6df98c98eede3ecf7a6f6e259f
Washington Carlos <wash_28@outlook.com>
2021-03-28 19:27:10 -0300

    atualizaçao das telas 3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 @extends('principal')
 @section('content')
     <form class="form-signin" method ="POST" action = "valida_login.php">
-        <img class="mb-4" src="image/ube12.png" alt="seção de manutenção" width = 180 height = 90>
+        <img class="mb-4" src="{{url('/image/ube12.png')}}" alt="seção de manutenção" width = 180 height = 90>
         <h1 class="h3 mb-3 font-weight-normal">Faça o login</h1>
         <label for="inputNumber" class="sr-only">CPF</label>
         <input type="text" id="inputNumber"  class="form-control" name="cpf" placeholder="CPF - somente os numeros" onkeypress="return onlynumber();" required autofocus>
@@ -15,6 +15,6 @@
         <button class="btn btn-lg btn-success btn-block" type="submit">Login</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2020 - Seção de Manutenção</p>      
         <p class = "text-center text-danger" >
-      </p>
+        </p>
     </form>
 @stop
